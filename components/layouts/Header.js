4dc0d3a7b264427fb40ad2ebba1b089713a6cbd9
Adam Bergman <adam.bergman@bontouch.com>
2021-12-20 11:14:01 +0100

    add active class to current nav item

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '../../styles/Header.module.css';
+import { useRouter } from 'next/router';
 
 const LINKS = [
   {
@@ -18,19 +20,30 @@ const LINKS = [
 ];
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <header className='p-3 bg-dark'>
       <div className='container'>
         <nav className='navbar navbar-expand-lg navbar-dark'>
           <Link href='/'>
-            <a className='navbar-brand logo'>BonPing</a>
+            <a className='navbar-brand logo'>
+              <Image src='/pingpong.svg' width='18' height='18' />
+              <span className='ms-2'>PingBon</span>
+            </a>
           </Link>
 
           <ul className={`navbar-nav ms-md-auto ${styles.nav}`}>
             {LINKS.map((link) => (
               <li className='nav-item' key={link.name}>
                 <Link href={link.path}>
-                  <a className='nav-link'>{link.name}</a>
+                  <a
+                    className={`nav-link ${
+                      router.pathname === link.path ? 'active' : ''
+                    }`}
+                  >
+                    {link.name}
+                  </a>
                 </Link>
               </li>
             ))}
